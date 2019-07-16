ed401bb6e607eb7f120184a727726cc4ce9872c4
hyppolitte djouda <hyppolitte.djouda@outlook.com>
2019-07-16 15:39:46 -0400

    initial commit

---
+++ ARRAYS/ARRAYS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAYS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] xs = new int[7] {1, 2, 3, 4, 5, 6, 7};
+            var xs = new List<string>() { "cats", "dogs", "horses", "pigs" };
+
+            
+          for ( int i=0; i< xs.Count -1; ++i)
+            {
+                xs[i] = xs[i + 1];
+            }
+
+
+            foreach (var x in xs)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
